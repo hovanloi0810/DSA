b39ba8c4e3727a7de0dc5e97eeda997f696c3716
hovanloi <hovanloi0810@gmail.com>
2023-12-25 23:04:55 +0700

    Problem 1 and 4 fixed

--- homework.cpp
+++ homework.cpp
@@ -10,17 +10,17 @@ int main()
 {
   // Bài 1
   std::string myString1;
-  std::cin >> myString1;
+  std::getline(std::cin, myString1);
   std::cout << myString1 << std::endl;
-  
+
   // Bài 2
   std::string myString = "Hello world";
   std::cout << myString << std::endl;
   
   // Bài 3
-  std::string firstString = "Hello ";
+  std::string firstString = "Hello";
   std::string lastString = "world";
-  std::string fullString = firstString + lastString;
+  std::string fullString = firstString + " " + lastString;
   std::cout << fullString << std::endl;
   
   // Bài 4
